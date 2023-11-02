57f028307d4d0c6217521d72d6083dd0f5c2522e
harrisonkaiser <hxk1633@rit.edu>
2023-11-02 19:08:02 -0400

    Initial Commit

---
+++ ASHA/ASHAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASHAApp.swift
+//  ASHA
+//
+//  Created by Harrison Kaiser on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ASHAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
